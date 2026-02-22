f1fd24f66efc4be3082ebbae55b25970e3b74411
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-02-22 19:59:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (70.75%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+
+        int prev = -1;
+
+        int result = 0;
+
+        for (int curr = 0; curr < 32; curr++) {
+            if (((n >> curr) & 1) != 0) {
+                result = (prev != -1) ? max(result, curr - prev) : result;
+                prev = curr;
+            }
+        }
+
+        return result;
+    }
+};
