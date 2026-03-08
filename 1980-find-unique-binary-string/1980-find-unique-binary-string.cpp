2bf1bfff0560dde39d68bc4876bd5c6303c2a3e6
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-03-08 13:50:25 +0530

    Time: 0 ms (100%), Space: 12.7 MB (55.15%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        int n = nums.size();
+        string result = "";
+
+        for (int i = 0; i < n; i++) {
+            char ch = nums[i][i];
+
+            result += (ch == '1') ? '0' : '1';
+        }
+        return result;
+    }
+};
