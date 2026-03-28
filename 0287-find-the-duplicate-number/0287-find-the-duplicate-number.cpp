4bdab09949ee360678759ace1dcf9cf4f6e17273
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-03-28 12:16:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        unordered_set<int> st;
         for (int i = 0; i < nums.size(); i++) {
-            if (st.find(nums[i]) != st.end()) {
-                return nums[i];
-            }
-            else{
-                st.insert(nums[i]);
-            }
+            int idx = abs(nums[i]);
+
+            if (nums[idx] < 0)
+                return idx;
+
+            nums[idx] = -nums[idx];
         }
-        return 0;
+        return -1;
     }
 };
