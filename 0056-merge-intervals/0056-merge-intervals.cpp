a5d9aa6b35dca11318521b2baaae76aa51eb0507
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-03-28 12:57:12 +0530

    Time: 2 ms (92.83%), Space: 23.9 MB (31.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,7 +8,7 @@ public:
         result.push_back({intervals[0][0], intervals[0][1]});
         for(int i = 1;i < intervals.size();i++){
             if(result.back()[1] >= intervals[i][0]){
-                result.back()[1] = intervals[i][1];
+                result.back()[1] = max(result.back()[1], intervals[i][1]);
             }
             else{
                 result.push_back({intervals[i][0], intervals[i][1]});
