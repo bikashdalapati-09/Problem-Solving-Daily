751e53d27110752acaaf974d1dfcca0c55947371
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-04-17 14:12:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,23 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int currSum, int target) {
+    int solve(vector<int>& nums, int i, int currSum, int target, vector<vector<int>>& dp) {
         if (i == nums.size()) {
-            if (currSum == target) {
-                return 1;
-            }
-            return 0;
+            return currSum == target ? 1 : 0;
         }
 
-        int plus = solve(nums, i + 1, currSum + nums[i], target);
-        int minus = solve(nums, i + 1, currSum - nums[i], target);
+        if (dp[i][currSum + 1000] != -1) {
+            return dp[i][currSum + 1000];
+        }
 
-        return plus + minus;
+        int plus = solve(nums, i + 1, currSum + nums[i], target, dp);
+        int minus = solve(nums, i + 1, currSum - nums[i], target, dp);
+
+        return dp[i][currSum + 1000] = plus + minus;
     }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(nums, 0, 0, target);
+        int n = nums.size();
+        vector<vector<int>> dp(n, vector<int>(2001, -1));
+        return solve(nums, 0, 0, target, dp);
     }
 };
