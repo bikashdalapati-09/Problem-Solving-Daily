6d1856bb5384ea7c4499565094dd6bdf0af01718
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-04-20 13:28:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,32 +1,37 @@
 class Solution {
-public: 
-    bool solve(string &s, int l, int r){
-        if(l >= r) 
+public:
+    bool solve(string& s, int l, int r, vector<vector<int>>& dp) {
+        if (l >= r)
             return 1;
 
-        if(s[l] == s[r]) {
-            return solve(s, l+1, r-1);
+        if (dp[l][r] != -1) {
+            return dp[l][r];
         }
 
-        return false;
+        if (s[l] == s[r]) {
+            return dp[l][r] = solve(s, l + 1, r - 1, dp);
+        }
+
+        return dp[l][r] = false;
     }
-    
+
     string longestPalindrome(string s) {
         int n = s.length();
-        
+
         int maxlen = INT_MIN;
         int startingIndex = 0;
 
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                
-                if(solve(s, i, j)) {
-                    if(j-i+1 > maxlen){
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+
+                if (solve(s, i, j, dp)) {
+                    if (j - i + 1 > maxlen) {
                         startingIndex = i;
-                        maxlen = j-i+1;
+                        maxlen = j - i + 1;
                     }
                 }
-                
             }
         }
 
