0672ca8c6c59f55f8fcdf945df8661df0c6757f9
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-04-20 13:26:38 +0530

    Time: 849 ms (9.24%), Space: 9.3 MB (81.1%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,35 @@
 class Solution {
-public:
-    bool isPailndrome(string s) {
-        string s2 = s;
-        reverse(s2.begin(), s2.end());
+public: 
+    bool solve(string &s, int l, int r){
+        if(l >= r) 
+            return 1;
 
-        return s == s2;
+        if(s[l] == s[r]) {
+            return solve(s, l+1, r-1);
+        }
+
+        return false;
     }
+    
     string longestPalindrome(string s) {
-        int n = s.size();
-        int len = 0;
-        string res = "";
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                string temp = s.substr(i, j);
-                if (isPailndrome(temp)) {
-                    if (len < temp.size()) {
-                        len = temp.size();
-                        res = temp;
+        int n = s.length();
+        
+        int maxlen = INT_MIN;
+        int startingIndex = 0;
+
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                
+                if(solve(s, i, j)) {
+                    if(j-i+1 > maxlen){
+                        startingIndex = i;
+                        maxlen = j-i+1;
                     }
                 }
+                
             }
         }
-        return res;
+
+        return s.substr(startingIndex, maxlen);
     }
 };
