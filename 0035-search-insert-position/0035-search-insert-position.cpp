e744a3c87e68bfb040e23bf269e16f71d9dc9bc1
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-05-04 20:45:45 +0530

    Time: 0 ms (100%), Space: 13.7 MB (41.56%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+
+        int i = 0;
+        int j = n - 1;
+
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return i;
+    }
+};
