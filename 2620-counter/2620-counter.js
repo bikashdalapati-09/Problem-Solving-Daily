33ee678968f859b88b6d55e8436bfd47a9b5a4bd
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-05-15 01:34:44 +0530

    Time: 35 ms (90.21%), Space: 53.7 MB (39.54%) - LeetHub

--- 2620-counter/2620-counter.js
+++ 2620-counter/2620-counter.js
@@ -3,10 +3,9 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-    let count = n-1;
+    let count = n;
     return function() {
-        count += 1;
-        return count;
+        return count++;
     };
 };
 
