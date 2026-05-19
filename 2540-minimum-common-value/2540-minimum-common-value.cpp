ab0b04eca2211d35713dfe6e83c2c8816eee937e
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-05-19 13:04:35 +0530

    Time: 39 ms (22.8%), Space: 69.7 MB (14.31%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        unordered_set<int> st;
+
+        for(auto i: nums1){
+            st.insert(i);
+        }
+
+        for(auto i: nums2){
+            if(st.find(i) != st.end()){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
