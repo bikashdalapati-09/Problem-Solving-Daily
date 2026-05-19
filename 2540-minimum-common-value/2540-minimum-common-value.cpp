3411f6c4ca1e6a2fcdbac21052fe27cae7c15fd5
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-05-19 13:15:03 +0530

    Time: 3 ms (38.92%), Space: 54.6 MB (30.11%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
-    int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        unordered_set<int> st;
+    bool binarySearch(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size() - 1;
 
-        for(auto i: nums1){
-            st.insert(i);
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return nums[mid];
+            }
+
+            if (nums[mid] < target) {
+                l = mid + 1;
+            }
+
+            else {
+                r = mid - 1;
+            }
         }
+        return false;
+    }
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
 
-        for(auto i: nums2){
-            if(st.find(i) != st.end()){
+        for (auto i : nums1) {
+            if (binarySearch(nums2, i)) {
                 return i;
             }
         }
+
         return -1;
     }
 };
