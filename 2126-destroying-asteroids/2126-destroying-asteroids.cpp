d63bb27783487125ef571143cf5ad3ba61368e8b
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-05-31 20:36:09 +0530

    Time: 23 ms (93.28%), Space: 106.8 MB (16.2%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
-        int n = asteroids.size();
-        if (n == 1) {
-            return mass >= asteroids[0];
-        }
+        sort(begin(asteroids), end(asteroids));
 
-        sort(asteroids.begin(), asteroids.end());
-        if(mass < asteroids[0]) {
-            return false;
-        }
+        long long totalMass = mass;
 
-        long long sum = mass;
-
-        for (int i = 0; i < n - 1; i++) {
-            sum += asteroids[i];
-            if (sum < asteroids[i + 1]) {
+        for(int &asteroid : asteroids) {
+            if(totalMass < asteroid)
                 return false;
-            }
+
+            totalMass += asteroid;
         }
+
         return true;
     }
 };
