c8dbeecf929f36816a8ce3b849b0613c52ebf030
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-07-06 19:17:12 +0530

    Time: 0 ms (100%), Space: 32.2 MB (48.52%) - LeetHub

--- 3833-count-dominant-indices/3833-count-dominant-indices.cpp
+++ 3833-count-dominant-indices/3833-count-dominant-indices.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int dominantIndices(vector<int>& nums) {
         int n = nums.size();
-        int totalSum = 0, res = 0;
-        for (int i = 0; i < n; i++)
-            totalSum += nums[i];
+        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        int res = 0;
+
         for (int i = 0; i < n - 1; i++) {
             totalSum -= nums[i];
             int avg = totalSum / (n - i - 1);
