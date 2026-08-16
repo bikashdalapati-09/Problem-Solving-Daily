febe5d2c4a3b3fb9e727d81e1227953e65d608fd
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-08-16 14:32:41 +0530

    Time: 0 ms (100%), Space: 9 MB (18.16%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for(char ch : s){
+            if(st.empty()){
+                st.push(ch);
+            }
+            else if(st.top() == '[' && ch == ']'){
+                st.pop();
+            }
+            else if(st.top() == '{' && ch == '}'){
+                st.pop();
+            }
+            else if(st.top() == '(' && ch == ')'){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        return st.empty();
+    }
+};
