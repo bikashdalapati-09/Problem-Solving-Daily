a40c15ff83f6cf288f0c7f7b45f0695dda1f4e8f
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-08-26 14:37:33 +0530

    Time: 4 ms (15.36%), Space: 59.3 MB (37.79%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.cpp
@@ -4,13 +4,28 @@ public:
         int n = arr.size();
         int count = 0;
 
-        for(int i = 0;i <= n - k;i++){
-            int avg = accumulate(arr.begin() +  i, arr.begin() + i + k, 0) / k;
-            cout << avg << endl;
-            if(avg >= threshold){
-                count++;
+        int i = 0, j = 0;
+        int sum = 0;
+
+        while (j < n) {
+            sum += arr[j];
+
+            while (j - i + 1 > k) {
+                sum -= arr[i];
+                i++;
             }
+
+            if (j - i + 1 == k) {
+                int avg = sum / k;
+
+                if (avg >= threshold) {
+                    count++;
+                }
+            }
+
+            j++;
         }
+
         return count;
     }
 };
