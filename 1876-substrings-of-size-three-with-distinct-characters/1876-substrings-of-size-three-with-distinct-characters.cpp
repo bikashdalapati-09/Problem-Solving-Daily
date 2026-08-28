e830ea579ae2472158da723adaffdd4c5ae7724b
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-08-28 14:01:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (51%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
-    bool isSame(string s) {
-        vector<int> arr(26, 0);
-
-        for (auto i : s) {
-            arr[i - 'a']++;
-        }
-
-        for (auto i : arr) {
-            if (i > 1) {
-                return false;
-            }
-        }
-        return true;
-    }
     int countGoodSubstrings(string s) {
-        int n = s.size();
         int count = 0;
 
-        int i = 0;
-        int j = 0;
-
-        while (j < n) {
-
-            if (j - i + 1 > 3) {
-                i++;
+        for (int i = 0; i + 2 < s.size(); i++) {
+            if (s[i] != s[i + 1] &&
+                s[i] != s[i + 2] &&
+                s[i + 1] != s[i + 2]) {
+                count++;
             }
-            if (j - i + 1 == 3) {
-                string str = s.substr(i, 3);
-                cout << str << endl;
-                if (isSame(str)) {
-                    count++;
-                }
-            }
-            j++;
         }
+
         return count;
     }
 };
