9781a8606a2d5b1d7d202e9899a48eb5bc1d7442
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-09-21 13:41:38 +0530

    Time: 56 ms (50.95%), Space: 92.1 MB (97.26%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -1,39 +1,40 @@
 class Solution {
 public:
-    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        int n = items.size();
+    vector<int> maximumBeauty(vector<vector<int>>& items,
+                              vector<int>& queries) {
+        int m = items.size();
+        int n = queries.size();
 
         sort(items.begin(), items.end());
 
         int maxBeauty = 0;
 
-        for(int i = 0; i < n; i++){
+        for (int i = 0; i < m; i++) {
             maxBeauty = max(maxBeauty, items[i][1]);
             items[i][1] = maxBeauty;
         }
 
-        vector<int> ans;
+        vector<int> ans(n, 0);
+        for (int i = 0; i < n; i++) {
 
-        for(int query : queries){
+            int query = queries[i];
+
             int l = 0;
-            int r = n - 1;
-            int beauty = 0;
+            int r = m - 1;
 
-            while(l <= r){
+            while (l <= r) {
                 int mid = l + (r - l) / 2;
 
-                if(items[mid][0] <= query){
-                    beauty = items[mid][1];
+                if (items[mid][0] <= query) {
+                    ans[i] = max(ans[i], items[mid][1]);
                     l = mid + 1;
                 }
-                else{
+
+                else {
                     r = mid - 1;
                 }
             }
-
-            ans.push_back(beauty);
         }
-
         return ans;
     }
 };
