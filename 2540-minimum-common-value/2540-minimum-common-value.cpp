b20a8e39729cf59530dcd216d62e2b85d76e3908
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-09-26 14:19:21 +0530

    Time: 0 ms (100%), Space: 54.5 MB (55.26%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -7,14 +7,14 @@ public:
         int i = 0;
         int j = 0;
 
-        while (i < m && j < n) {
-            if (nums1[i] == nums2[j]) {
+        while(i < m && j < n){
+            if(nums1[i] == nums2[j]){
                 return nums1[i];
             }
-
-            if (nums1[i] < nums2[j]) {
+            else if(nums1[i] < nums2[j]){
                 i++;
-            } else {
+            }
+            else{
                 j++;
             }
         }
