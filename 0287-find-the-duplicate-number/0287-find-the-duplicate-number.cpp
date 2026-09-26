28ba7a203ab075dccd370a13e5834446ef844677
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-09-26 15:03:30 +0530

    Time: 3 ms (68.61%), Space: 65.1 MB (69.52%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -4,20 +4,18 @@ public:
         int slow = nums[0];
         int fast = nums[0];
 
-        slow = nums[slow];
-        fast = nums[nums[fast]];
-
-        while (slow != fast) {
+        do {
             slow = nums[slow];
             fast = nums[nums[fast]];
-        }
+
+        } while (slow != fast);
 
         slow = nums[0];
+
         while (slow != fast) {
             slow = nums[slow];
             fast = nums[fast];
         }
-
         return slow;
     }
 };
